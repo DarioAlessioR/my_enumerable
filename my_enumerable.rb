36880780e251e01d1596432faa265c26491d7657
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-09-01 15:05:25 -0400

    Add my_list and my_enumerable files

---
+++ my_enumerable.rb
@@ -0,0 +1,21 @@
+module MyEnumerable
+  def all_method
+    list.each { |element| return false unless yield element }
+  
+    true
+  end
+
+  def any_method
+    list.each { |element| return true if yield element }
+
+    false
+  end
+
+  def filter_method
+    result = []
+    
+    list.each { |element| result.push(element) if yield element }
+
+    result
+  end
+end
